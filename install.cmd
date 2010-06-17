108de5bc21f301814013ee9eb11e5a0645f017d1
Mathias Eck <m99e99@gmail.com>
2010-06-17 10:46:54 +0200

    Version 0.1.0

--- install.cmd
+++ install.cmd
@@ -1,3 +1,5 @@
+set DEBUG=NO
+call pu.cmd
 call package-it.cmd
-call pdk-install com.vocshopgames.chess_0.0.5_all.ipk
-plink -P 10022 root@localhost -pw "" "chmod 755 /media/cryptofs/apps/usr/palm/applications/com.vocshopgames.chess/*"
+call pdk-install com.vocshopgames.chess_0.1.0_all.ipk
+plink -P 10022 root@localhost -pw "viktor" "chmod 755 /media/cryptofs/apps/usr/palm/applications/com.vocshopgames.chess/*"
--- package-it.cmd
+++ package-it.cmd
@@ -1,18 +1,26 @@
 set STAGING_DIR=STAGING\com.vocshopgames.chess
-
-
 rmdir /s /y %STAGING_DIR%
 del *.ipk
 mkdir %STAGING_DIR%
 xcopy /s /y mojo %STAGING_DIR%
+
+copy /b mojo\app\assistants\game-assistant_*.js %STAGING_DIR%\app\assistants\game-assistant1.js
+del %STAGING_DIR%\app\assistants\game-assistant_*.js
+
+find /v "Mojo.Log.error" <%STAGING_DIR%\app\assistants\game-assistant1.js  >%STAGING_DIR%\app\assistants\game-assistant.js 
+find /v "Mojo.Log.error" <mojo\app\assistants\stage-assistant.js  >%STAGING_DIR%\app\assistants\stage-assistant.js 
+find /v "Mojo.Log.error" <mojo\app\assistants\preferences-assistant.js  >%STAGING_DIR%\app\assistants\preferences-assistant.js
+::jsc %STAGING_DIR%\app\assistants\game-assistant.js  %STAGING_DIR%\app\assistants\game-assistant.js 
+::jsc %STAGING_DIR%\app\assistants\stage-assistant.js %STAGING_DIR%\app\assistants\stage-assistant.js 
+::jsc %STAGING_DIR%\app\assistants\preferences-assistant.js  %STAGING_DIR%\app\assistants\preferences-assistant.js
 :: cd engines
 :: call _buildit.cmd
 :: cd ..
 copy engines\polyglot\polyglot %STAGING_DIR%
-copy engines\glaurung\glaurung %STAGING_DIR%
+::copy engines\glaurung\glaurung %STAGING_DIR%
 copy engines\stockfish\stockfish %STAGING_DIR%
-copy engines\fruit\fruit %STAGING_DIR%
+::copy engines\fruit\fruit %STAGING_DIR%
 copy engines\polyglot.ini %STAGING_DIR%
-copy postinst %STAGING_DIR%
-echo filemode.755=polyglot,stockfish,glaurung,fruit > %STAGING_DIR%\package.properties
+::copy engines\book.bin %STAGING_DIR%
+echo filemode.755=polyglot,stockfish,glaurung,fruit,book.bin > %STAGING_DIR%\package.properties
 pdk-package %STAGING_DIR%
